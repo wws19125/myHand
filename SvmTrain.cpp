e9d58635cbe1048d6e403f2c4e3a261685db6d94
wang <wws19125@126.com>
2013-05-17 10:35:02 +0800

    branch svm init

--- SvmTrain.cpp
+++ SvmTrain.cpp
@@ -3,37 +3,50 @@
 #include<cv.h>
 #include<highgui.h>
 #include<ml.h>
-
+#include<vector>
 using namespace std;
 using namespace cv;
 
 class SvmTrain
 {
+public:
+  SvmTrain();
+private:
+  //attributes
+  vector<double> res;
+  //methods
+  //从文件中载入
+  void loadFile();
+  //从摄像头中获取训练样本
+  void getSampleFromCAM();
   
 };
-
+SvmTrain::SvmTrain()
+{
+  cout<<"holy shit"<<endl;
+  getchar();
+}
 int main()
 {
+  SvmTrain sv;
+  return 0;
   int i,j,ii,jj;
   int width = 28,height = 30;
   int image_dim = width * height;
   int pnum = 10;
   IplImage *img_org,*sample_img;
-  int res[11];
+  int res[10];
   float data[pnum*image_dim];
   CvMat data_mat,res_mat;
   CvSVM svm = CvSVM();
   CvTermCriteria criteria;
   CvSVMParams param;
   char filename[2];
-  cvNamedWindow("svm_org",CV_WINDOW_AUTOSIZE);
-  cvShowImage("svm_org",cvLoadImage("1.bmp"));
   for( i=0;i<pnum;i++)
     {
-      sprintf(filename,"%d.bmp",i+1,i+1);
-      cout<<(img_org = cvLoadImage(filename,CV_LOAD_IMAGE_GRAYSCALE))<<endl;
-      //cvShowImage("svm_org",img_org);
-      //cout<<"load "<<filename<<endl;
+      sprintf(filename,"handp/p%d/%d%d.png",i+1,i+1,i+1);
+      cout<<filename<<" ";
+      img_org = cvLoadImage(filename,CV_LOAD_IMAGE_GRAYSCALE);
       sample_img = cvCreateImage(cvSize(width,height),IPL_DEPTH_8U,1);
       cvResize(img_org,sample_img);
       cvSmooth(sample_img,sample_img,CV_GAUSSIAN,3,0,0,0);
@@ -44,14 +57,15 @@ int main()
 	      data[ i * image_dim+(ii*width)+jj]=float((int)((uchar)(sample_img->imageData[ii*sample_img->widthStep+jj]))/255.0);
 	    }
 	}
-      res[i]=1;
-      cout<<img_org<<"      "<<filename<<endl;
-      getchar();
+      res[i]=(i+1)%2;
+      cout<<res[i]<<endl;
     }
+
   cvInitMatHeader(&data_mat,pnum,image_dim,CV_32FC1,data);
   cvInitMatHeader(&res_mat,pnum,1,CV_32FC1,res);
-  criteria = cvTermCriteria(CV_TERMCRIT_EPS,1000,FLT_EPSILON);
-  param = CvSVMParams(CvSVM::NU_SVR,CvSVM::RBF,10.0,0.09,1.0,10.0,0.2,1.0,NULL,criteria);
+  criteria = cvTermCriteria(CV_TERMCRIT_EPS,100,FLT_EPSILON);
+  param = CvSVMParams(CvSVM::C_SVC,CvSVM::RBF, 10.0, 8.0, 1.0, 10.0, 0.02, 1.0,NULL,criteria);
+  cout<<"--------------------"<<endl;
   svm.train(&data_mat,&res_mat,NULL,NULL,param);
   svm.save("svm_image.xml");
   //cvReleaseImage(&img_org);
